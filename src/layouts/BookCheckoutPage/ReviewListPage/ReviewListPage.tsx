9e2ffa300379b4c435c1d6dbebdbdb791783801c
Bora <bora@Bora-MacBook-Air.local>
2023-03-14 13:22:53 +0300

    implemented the ReviewListPage useEffect

--- src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
+++ src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReviewModel from '../../../models/ReviewModel';
 
 const ReviewListPage: React.FC = () => {
@@ -12,6 +12,48 @@ const ReviewListPage: React.FC = () => {
   const [totalPages, setTotalPages] = useState<number>(0);
 
   const bookId = window.location.pathname.split('/')[2];
+
+  useEffect(() => {
+    const fetchBookReviews = async () => {
+      const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}&page=${
+        currentPage - 1
+      }&size=${reviewsPerPage}`;
+
+      const responseReviews = await fetch(reviewUrl);
+
+      if (!responseReviews.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseJSON = await responseReviews.json();
+
+      const responseData = responseJSON._embedded.reviews;
+
+      setTotalAmountOfReviews(responseJSON.page.totalElements);
+      setTotalPages(responseJSON.page.totalPages);
+
+      const loadedReviews: ReviewModel[] = [];
+
+      for (const key in responseData) {
+        loadedReviews.push({
+          id: responseData[key].id,
+          userEmail: responseData[key].userEmail,
+          date: responseData[key].date,
+          rating: responseData[key].rating,
+          book_id: responseData[key].book_id,
+          reviewDescription: responseData[key].reviewDescription,
+        });
+      }
+
+      setReviews(loadedReviews);
+      setIsLoading(false);
+    };
+
+    fetchBookReviews().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, [currentPage]);
 
   return <div>ReviewListPage</div>;
 };
