66faf6a09d6606cc61fcdd2a5ed2860725cbedc0
Bora <bora@Bora-MacBook-Air.local>
2023-03-15 13:49:27 +0300

    created MessagesPage component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget.jsx';
 import ReviewListPage from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import ShelfPage from './layouts/ShelfPage/ShelfPage';
+import MessagesPage from './layouts/MessagePage/MessagesPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -56,6 +57,9 @@ const App = () => {
             <Route path='/login/callback' component={LoginCallback} />
             <SecureRoute path='/shelf'>
               <ShelfPage />
+            </SecureRoute>
+            <SecureRoute path='/messages'>
+              <MessagesPage />
             </SecureRoute>
           </Switch>
         </div>
