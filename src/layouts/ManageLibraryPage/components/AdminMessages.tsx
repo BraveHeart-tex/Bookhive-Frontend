3dfa43d1f9dc5512e1917cf4500347ff4bb48c33
Bora <bora@Bora-MacBook-Air.local>
2023-03-15 19:35:29 +0300

    implemented fetching open tickets functionality

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -1,6 +1,7 @@
 import { useOktaAuth } from '@okta/okta-react';
 import { useEffect, useState } from 'react';
 import Spinner from '../../Utils/Spinner';
+import MessageModel from '../../../models/MessageModel';
 
 const AdminMessages = () => {
   const { authState } = useOktaAuth();
@@ -8,11 +9,36 @@ const AdminMessages = () => {
   const [isLoadingMessage, setIsLoadingMessages] = useState<boolean>(true);
   const [httpError, setHttpError] = useState(null);
 
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+  const [messagesPerPage] = useState(5);
+
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
-    const fetchUserMessages = async () => {};
+    const fetchUserMessages = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/messages/search/findByClosed/?closed=false&page=${
+          currentPage - 1
+        }&size=${messagesPerPage}`;
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        };
+        const messagesResponse = await fetch(url, requestOptions);
+        if (!messagesResponse.ok) {
+          throw new Error('Something went wrong...');
+        }
+
+        const messagesResponseJSON = await messagesResponse.json();
+        setMessages(messagesResponseJSON._embedded.messages);
+        setTotalPages(messagesResponseJSON.page.totalPages);
+      }
+      setIsLoadingMessages(false);
+    };
 
     fetchUserMessages().catch((error: any) => {
       setIsLoadingMessages(false);
