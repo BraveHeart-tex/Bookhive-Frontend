922a19737c2a2b99d428834ee337eb05ecc4fa0d
Bora <bora@Bora-MacBook-Air.local>
2023-03-19 15:54:02 +0300

    add stripe elements structure to payment page

--- src/layouts/PaymentPage/PaymentPage.tsx
+++ src/layouts/PaymentPage/PaymentPage.tsx
@@ -1,6 +1,8 @@
 import { useOktaAuth } from '@okta/okta-react';
 import { useEffect, useState } from 'react';
 import Spinner from '../Utils/Spinner';
+import { CardElement } from '@stripe/react-stripe-js';
+import { Link } from 'react-router-dom';
 
 const PaymentPage = () => {
   const { authState } = useOktaAuth();
@@ -47,6 +49,37 @@ const PaymentPage = () => {
     );
   }
 
-  return <div>PaymentPage</div>;
+  return (
+    <div className='container'>
+      {fees > 0 && (
+        <div className='card mt-3'>
+          <h5 className='card-header'>
+            Pending fees: <span className='text-danger'>${fees}</span>
+          </h5>
+          <div className='card-body'>
+            <h5 className='card-title mb-3'>Credit Card</h5>
+            <CardElement id='card-element' />
+            <button
+              disabled={submitDisabled}
+              type='button'
+              className='btn btn-md main color text-white mt-3 btn-dark'
+            >
+              Pay fees
+            </button>
+          </div>
+        </div>
+      )}
+
+      {fees === 0 && (
+        <div className='mt-3'>
+          <h5>You have no fees!</h5>
+          <Link type='button' className='btn main-color text-white' to='search'>
+            Explore our top books
+          </Link>
+        </div>
+      )}
+      {submitDisabled && <Spinner />}
+    </div>
+  );
 };
 export default PaymentPage;
