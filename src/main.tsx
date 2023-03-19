f1ba3fd80f9c75ef5e09daf29e8ea30362a402d7
Bora <bora@Bora-MacBook-Air.local>
2023-03-19 15:25:40 +0300

    add stripe cdn, stripe elements and PaymentInfoRequest model

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(
+  'pk_test_51MnLEfIKZGibl4EQJrbDHwRDXcCOu8w4hGxR7zGAkhOlB4JABOOydRgxagQSGoNkyBEPx0SIS0c5hsWZiv7Ev35Y00rEiGFm4v'
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
