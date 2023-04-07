bbb486dd3231a6c5f8d3aa5835f76b1e7862cd66
Bora <bora@Bora-MacBook-Air.local>
2023-04-07 18:47:07 +0300

    small fix

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-  clientId: '0oa8o9znnqpfKwxZt5d7',
-  issuer: 'https://dev-73647973.okta.com/oauth2/default',
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  issuer: process.env.REACT_APP_OKTA_ISSUER,
   redirectUri: 'https://bookhivetr.netlify.app/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true,
